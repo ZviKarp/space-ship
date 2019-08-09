a1bb18dd0f907e32eae74a43eee5f48171f8db48
r613 <icreatemyweb@gmail.com>
2019-08-08 20:50:40 -0400

    Add files via upload

--- game.py
+++ game.py
@@ -6,6 +6,55 @@ def gen_rock(brd,height,width):#returns a random location (as long as the locati
 		return [x,y]
 	else:
 		return gen_rock(brd,height,width)
+def rock_gen(brd,height,width):#returns a random location from the top of the map for a rock to come in 
+	y = 0
+	x = random.randint(1,width-2)
+	if brd[y][x] == " ":
+		return [x,y]
+	else:
+		return rock_gen(brd,height,width)
+def move_rocks(brd,height,width):
+	nlist = range(height)
+	nlist.reverse()
+	for r in nlist:
+		for c in range(width):
+			if brd[r][c] == 'O':
+				if safe_move_rock(brd,r,c,height):
+					pass
+				else:
+					return False
+	return True
+def safe_move_rock(brd,r,c,height):
+	brd[r][c] = ' '
+	if r == height-1:
+		try:
+			if brd[0][c] == '|':
+				pass
+			elif brd[0][c] == 'O':
+				pass
+			elif brd[0][c] == ' ':
+				brd[0][c] = 'O'
+			else:#hit the spaceship
+				return False
+			return True
+		except:
+			pass
+	else:
+		try:
+			if brd[r+1][c] == '|':
+				pass
+			elif brd[r+1][c] == 'O':
+				pass
+			elif brd[r+1][c] == ' ':
+				brd[r+1][c] = 'O'
+			else:#hit the spaceship
+				return False
+			return True
+		except:
+			pass
+	return True
+
+
 def move_lasers(brd,height,width):
 	for r in range(height):
 		for c in range(width):
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import keyboard 
 import time
 from user import Input,Print,press
-from game import gen_rock
-from game import move_lasers
+from game import gen_rock,rock_gen
+from game import move_lasers,move_rocks
 from bordm import con,insert_rock,insert_ship
 from moves import move_right,move_left,move_up,move_down
 height = 20
 width =  40
 brd = []
-ship = [(width-1)/2,(height-1)] #ship rules: cannot be less than 1 away from edge (half of the ship is to the right of it's actual location) (both edges)
+ship = [(width-1)/2,(height-2)] #ship rules: cannot be less than 1 away from edge (half of the ship is to the right of it's actual location) (both edges)
 speed = float(Input("Please enter the speed of the game 'higher number-slower game' "))
 c = " "
-rocks = [[(width/2),(height/2)]]
+#rocks = [[(width/2),(height/2)]]
+rocks = []
+rocket_moves = 2#per rock move
+rock_move = 2#per rock regeneration
 
 def move(ship,height,width,rocks,brd,c):#ship - coordinants of the ship, height and width - of the board, rocks - list of the coordinants of the rocks, brd - matrix with char's resembling what's in that place of the board,c - character resembling in what direction the player is moving 
     if c == "r":
@@ -26,24 +29,31 @@ def move(ship,height,width,rocks,brd,c):#ship - coordinants of the ship, height 
         return False
     else:
         return brd
-
-    
-
+def las(brd,height,width):
+    for c in range(width):
+        brd[height-1][c] = '|'
+    return 0
+#time.sleep(5)
 brd = con(ship,height,width,rocks)
 while True:
-    for i in range(10):
+    for i in range(rock_move):
         time.sleep(speed/100)
-        
-        c = press(c)
-        brd = move(ship,height,width,rocks,brd,c)
-        move_lasers(brd,height,width)
-        insert_ship(brd,ship)
-        c = " "
+        for l in range(rocket_moves):
+            c = press(c)
+            if keyboard.is_pressed('r'):
+                las(brd,height,width)
+            brd = move(ship,height,width,rocks,brd,c)
+            move_lasers(brd,height,width)
+            insert_ship(brd,ship)
+            c = " "
         if brd == False:
             print ("You Lost!")
-            break
-        
+            exit() 
         Print(brd)
+        if move_rocks(brd,height,width) == False:#moves rocks
+            print "haah"
+            print ("You Lost!")
+            exit()
     
-    insert_rock(brd,gen_rock(brd,height,width))
+    insert_rock(brd,rock_gen(brd,height,width))
 print ("Was a great game")
